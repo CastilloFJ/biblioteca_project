d1958f988c2f692ce0f536c0d4fcac7cd87ea207
CastilloFJ <jose.castf@gmail.com>
2026-03-28 18:47:18 -0300

    Inicial commit: Proyecto Biblioteca con Django

---
+++ biblioteca/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "biblioteca"
+
+urlpatterns = [
+    path("libros/", views.lista_libros, name="lista_libros"),
+    path("libros/<int:libro_id>/", views.detalle_libro, name="detalle_libro"),
+    
+]
